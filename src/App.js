17624e692f9a53c8e1a1276118b21db6dce1a749
Mahdi Khoshzat <92544225+Mahdikhoshzat@users.noreply.github.com>
2022-09-11 20:05:22 +0430

    ProductProject

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+
+import Products from './components/Products/Products'
+import Auth from './components/Auth'
+import { AuthContext } from './context/auth-context'
+import useDarkMode from './hooks/dark-mode'
+
+import './App.css'
+
+const App = (props) => {
+  const [theme, toggleTheme] = useDarkMode()
+
+  const authContext = useContext(AuthContext)
+
+  let content = <Auth />
+
+  if (authContext.isAuth) {
+    content = (
+      <div
+        className="app"
+        style={{
+          background: theme === 'dark' ? '#212121' : '#ffffff',
+          color: theme === 'dark' ? '#ffffff' : '#212121',
+          transition: '.2s all',
+          textAlign: 'center',
+        }}
+      >
+        <Products />
+        <button type="button" onClick={toggleTheme}>
+          تغیر تم
+        </button>
+      </div>
+    )
+  }
+
+  return content
+}
+
+export default App
---
+++ src/components/Products/Products.js
@@ -0,0 +1,52 @@
+import React, { useCallback, useReducer } from 'react'
+
+import ProductForm from './ProductForm'
+import ProductList from './ProductList'
+import Search from './Search'
+
+const productReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.products
+    case 'ADD':
+      return [...state, action.product]
+    default:
+      throw new Error('Error')
+  }
+}
+
+const Products = () => {
+  const [products, dispath] = useReducer(productReducer, [])
+
+  const searchProductsHandler = useCallback((items) => {
+    dispath({ type: 'SET', products: items })
+  }, [])
+
+  const addProductHandler = (item) => {
+    fetch('url', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((response) => {
+      response.json().then((responseData) => {
+        dispath({
+          type: 'ADD',
+          product: { id: responseData.name, ...item },
+        })
+      })
+    })
+  }
+
+  return (
+    <div className="App">
+      <ProductForm onAddProduct={addProductHandler} />
+
+      <section>
+        <Search onLoadProducts={searchProductsHandler} />
+        <ProductList products={products} onRemoveItem={() => {}} />
+      </section>
+    </div>
+  )
+}
+
+export default Products
